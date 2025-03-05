127f8eb052dee7a22df9e3205fba859eb8cbd38f
Mario Eggimann <eggimann@t-online.de>
2025-03-05 15:39:24 +0100

    Initial Commit

---
+++ SportsTransfer/SportsTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsTransferApp.swift
+//  SportsTransfer
+//
+//  Created by Mario Eggimann on 05.03.25.
+//
+
+import SwiftUI
+
+@main
+struct SportsTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SportsTransferTests/SportsTransferTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportsTransferTests.swift
+//  SportsTransferTests
+//
+//  Created by Mario Eggimann on 05.03.25.
+//
+
+import Testing
+@testable import SportsTransfer
+
+struct SportsTransferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
